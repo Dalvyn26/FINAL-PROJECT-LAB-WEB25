0283b80456a41e69cce9203ace26fb322301968f
Dalvyn Suhada <suhada.dalvyn@gmail.com>
2025-11-21 05:53:48 +0800

    "fix(backend): import DB facade to resolve internal server error"

--- manager_cuti/app/Http/Controllers/LeaveRequestController.php
+++ manager_cuti/app/Http/Controllers/LeaveRequestController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Services\LeaveRequestService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
 
